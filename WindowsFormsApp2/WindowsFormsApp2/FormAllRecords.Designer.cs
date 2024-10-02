be28633d3b8a3e092d54785bc8107a1b177f13ef
ven7ix <0olistiko0@gmail.com>
2024-10-02 21:27:33 +0300

    Add files via upload

--- WindowsFormsApp2/WindowsFormsApp2/FormAllRecords.Designer.cs
+++ WindowsFormsApp2/WindowsFormsApp2/FormAllRecords.Designer.cs
@@ -30,7 +30,12 @@
         {
             this.buttonNewRecord = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.buttonNewDependence = new System.Windows.Forms.Button();
+            this.buttonDeleteDependence = new System.Windows.Forms.Button();
+            this.buttonAddCell = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // buttonNewRecord
@@ -41,27 +46,70 @@
             this.buttonNewRecord.TabIndex = 0;
             this.buttonNewRecord.Text = "add";
             this.buttonNewRecord.UseVisualStyleBackColor = true;
-            this.buttonNewRecord.Click += new System.EventHandler(this.buttonNewRecord_Click);
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(89, 60);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(635, 220);
+            this.dataGridView1.Size = new System.Drawing.Size(240, 220);
             this.dataGridView1.TabIndex = 1;
-            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_RowHeaderMouseClick);
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellEndEdit);
+            this.dataGridView1.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.DataGridView1_RowHeaderMouseClick);
+            // 
+            // buttonNewDependence
+            // 
+            this.buttonNewDependence.Location = new System.Drawing.Point(335, 286);
+            this.buttonNewDependence.Name = "buttonNewDependence";
+            this.buttonNewDependence.Size = new System.Drawing.Size(242, 115);
+            this.buttonNewDependence.TabIndex = 2;
+            this.buttonNewDependence.Text = "add new cell inside";
+            this.buttonNewDependence.UseVisualStyleBackColor = true;
+            this.buttonNewDependence.Click += new System.EventHandler(this.ButtonNewDependence_Click);
+            // 
+            // buttonDeleteDependence
+            // 
+            this.buttonDeleteDependence.Location = new System.Drawing.Point(583, 286);
+            this.buttonDeleteDependence.Name = "buttonDeleteDependence";
+            this.buttonDeleteDependence.Size = new System.Drawing.Size(150, 115);
+            this.buttonDeleteDependence.TabIndex = 3;
+            this.buttonDeleteDependence.Text = "delete dependence";
+            this.buttonDeleteDependence.UseVisualStyleBackColor = true;
+            this.buttonDeleteDependence.Click += new System.EventHandler(this.ButtonDeleteDependence_Click);
+            // 
+            // buttonAddCell
+            // 
+            this.buttonAddCell.Location = new System.Drawing.Point(335, 60);
+            this.buttonAddCell.Name = "buttonAddCell";
+            this.buttonAddCell.Size = new System.Drawing.Size(242, 115);
+            this.buttonAddCell.TabIndex = 4;
+            this.buttonAddCell.Text = "add cell into main";
+            this.buttonAddCell.UseVisualStyleBackColor = true;
+            this.buttonAddCell.Click += new System.EventHandler(this.ButtonAddCell_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(583, 60);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(150, 220);
+            this.dataGridView2.TabIndex = 5;
             // 
             // FormAllRecords
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.buttonAddCell);
+            this.Controls.Add(this.buttonDeleteDependence);
+            this.Controls.Add(this.buttonNewDependence);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.buttonNewRecord);
             this.Name = "FormAllRecords";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -70,6 +118,10 @@
 
         private System.Windows.Forms.Button buttonNewRecord;
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button buttonNewDependence;
+        private System.Windows.Forms.Button buttonDeleteDependence;
+        private System.Windows.Forms.Button buttonAddCell;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
 
--- WindowsFormsApp2/WindowsFormsApp2/FormAllRecords.cs
+++ WindowsFormsApp2/WindowsFormsApp2/FormAllRecords.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using System.Threading;
 using System.Windows.Forms;
 using System.Xml.Linq;
 
@@ -11,48 +12,36 @@ namespace WindowsFormsApp2
         public FormAllRecords()
         {
             InitializeComponent();
-        }
 
-        List<CustomCell> people = new List<CustomCell>();
-
-        List<CustomCell> displayedCells = new List<CustomCell>();
-
-        private void buttonNewRecord_Click(object sender, EventArgs e)
-        {
+            //перенести в отдельный метод
             dataGridView1.Columns.Add("", "");
 
             Person person1 = new Person(1, "aaaaa", DateTime.Now);
             Person person2 = new Person(2, "bbbbb", DateTime.Now);
 
-            CustomCell person1Cell = addNewPersonInCell(person1);
-            CustomCell person2Cell = addNewPersonInCell(person2);
+            //убрать присваивание 
+            CustomCell person1Cell = AddNewPersonInCell(person1);
+            CustomCell person2Cell = AddNewPersonInCell(person2);
 
-            
 
-            CustomCell testOpen = new CustomCell("test testOpen", true, null);
-            CustomCell testOpenD1 = new CustomCell("test testOpenD1", true, testOpen);
-            CustomCell testOpenD2 = new CustomCell("test testOpenD2", false, testOpen);
-            CustomCell testOpenD1D1 = new CustomCell("test testOpenD1D1", false, testOpen);
-
-            person1Cell._dependentCells.Add(testOpen);
-
-            testOpen._dependentCells = new List<CustomCell>
-            {
-                testOpenD1,
-                testOpenD2
-            };
-
-            testOpenD1._dependentCells = new List<CustomCell>
-            {
-                testOpenD1D1
-            };
+            dataGridView2.Columns.Add("", "");
+            dataGridView2.Rows.Add("tesst");
         }
 
-        private CustomCell addNewPersonInCell(Person person)
+        List<CustomCell> people = new List<CustomCell>();
+        List<CustomCell> displayedCells = new List<CustomCell>();
+        List<CustomCell> allCells = new List<CustomCell>();
+
+        private CustomCell AddNewPersonInCell(Person person)
         {
             CustomCell main = new CustomCell(person._name, true, null);
             CustomCell dependentCell1 = new CustomCell(person._cardNumber.ToString(), false, main);
             CustomCell dependentCell2 = new CustomCell(person._birthday.ToString(), false, main);
+
+            //надо для запоминания изменения информации
+            allCells.Add(main);
+            allCells.Add(dependentCell1);
+            allCells.Add(dependentCell2);
 
             main._dependentCells = new List<CustomCell>
             {
@@ -67,14 +56,108 @@ namespace WindowsFormsApp2
             return main;
         }
 
-        private void dataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        private void ButtonAddCell_Click(object sender, EventArgs e)
         {
-            dataGridView1_OpenCellHandler(dataGridView1.SelectedRows[0].Index);
-
+            CustomCell cell = new CustomCell("no data", false, null);
+            allCells.Add(cell);
+            displayedCells.Add(cell);
+            dataGridView1.Rows.Add(cell._data);
             dataGridView1.Refresh();
         }
 
-        private void dataGridView1_OpenCellHandler(int selected)
+        private void NewDependence(CustomCell cell)
+        {
+            if (!cell._canBeOpened)
+            {
+                cell._dependentCells = new List<CustomCell>();
+                cell._canBeOpened = true;
+            }
+            
+            CustomCell testOpen = new CustomCell("test testOpen", false, cell);
+            allCells.Add(cell);
+
+            //закомментированное надо было для теста вложенности, сейчас вложенность работает сама
+            /*CustomCell testOpenD1 = new CustomCell("test testOpenD1", true, testOpen);
+            CustomCell testOpenD2 = new CustomCell("test testOpenD2", false, testOpen);
+            CustomCell testOpenD1D1 = new CustomCell("test testOpenD1D1", false, testOpen);
+
+            testOpen._dependentCells = new List<CustomCell>
+            {
+                testOpenD1,
+                testOpenD2
+            };
+
+            testOpenD1._dependentCells = new List<CustomCell>
+            {
+                testOpenD1D1
+            };*/
+
+            cell._dependentCells.Add(testOpen);
+            dataGridView1.Refresh();
+        }
+
+        private void ButtonNewDependence_Click(object sender, EventArgs e)
+        {
+            NewDependence(displayedCells[dataGridView1.CurrentCell.RowIndex]);
+            dataGridView1.Refresh();
+        }
+
+        private void DeleteDependence(int selected)
+        {
+            CustomCell cell = displayedCells[selected];
+
+            var queue = new Queue<CustomCell>();
+            queue.Enqueue(cell);
+
+
+            while (queue.Count > 0)
+            {
+                var current = queue.Dequeue();
+
+                //body
+
+
+                if (current._isShown && displayedCells.IndexOf(current) != selected)
+                {
+                    dataGridView1.Rows.RemoveAt(selected + 1);
+                    displayedCells.RemoveAt(selected + 1);
+                    
+                    current._isShown = false;
+                }
+
+                //end
+
+                if (current._dependentCells == null) continue;
+                foreach (CustomCell c in current._dependentCells)
+                {
+                    queue.Enqueue(c);
+                }
+            }
+
+            cell._canBeOpened = false;
+            cell._dependentCells = null;
+            // не работает, доделать
+            dataGridView1.Rows.RemoveAt(selected);
+            displayedCells.RemoveAt(selected);
+            //ломается т.к. у testOpen нет родитеся, хотя он в список детей у aaaaa
+            //вроде починил, но подумать как избавиться от parent
+
+            cell._parent?._dependentCells.Remove(cell);
+        }
+
+        private void ButtonDeleteDependence_Click(object sender, EventArgs e)
+        {
+            DeleteDependence(dataGridView1.CurrentCell.RowIndex);
+            dataGridView1.Refresh();
+        }
+
+        private void DataGridView1_RowHeaderMouseClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            DataGridView1_OpenCellHandler(dataGridView1.SelectedRows[0].Index);
+            dataGridView1.Refresh();
+        }
+
+        private void DataGridView1_OpenCellHandler(int selected)
         {
             CustomCell cell = displayedCells[selected];
 
@@ -129,42 +212,12 @@ namespace WindowsFormsApp2
             }
         }
 
-        /*int position = dataGridView1.SelectedRows[0].Index + 1;
-            int selected = dataGridView1.SelectedRows[0].Index;
-            
-
-
-            string str = dataGridView1.Rows[selected].Cells[0].Value.ToString();
-            if (str.Split(' ')[0] == "")
-            {
-                return;
-            }
-
-            if (!people[selected]._isOpen)
-            {
-                DataGridViewRow newRow1 = new DataGridViewRow();
-                newRow1.CreateCells(dataGridView1);
-
-                newRow1.Cells[0].Value = " :. ";
-
-                DataGridViewRow newRow2 = new DataGridViewRow();
-                newRow2.CreateCells(dataGridView1);
-                newRow2.Cells[0].Value = " :. ";
-
-                dataGridView1.Rows.Insert(position, newRow1);
-                dataGridView1.Rows.Insert(position + 1, newRow2);
-
-                people[selected]._isOpen = true;
-            }
-            else
-            {
-
-                for (int i = 0; i < 2; i++)
-                {
-                    dataGridView1.Rows.RemoveAt(position);
-                }
-                dataGridView1.Refresh();
-                people[selected]._isOpen = false;
-            }*/
+        private void DataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            int selected = dataGridView1.CurrentCell.RowIndex;
+            CustomCell cell = displayedCells[selected];
+            int cellIndex = cell._parent._dependentCells.IndexOf(cell);
+            cell._parent._dependentCells[cellIndex]._data = dataGridView1.Rows[selected].Cells[0].Value.ToString();
+        }
     }
 }
